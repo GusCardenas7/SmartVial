6bebbf1be0d5b4f402cee49d34f149574e6a388c
DianaGlez1512 <diana.gonzalez4379@alumnos.udg.mx>
2024-08-01 18:35:43 -0600

    Agregué colores diferentes al menú y realicé los Sliders de la página de inicio.

---
+++ Seguridad Vial/IndexSV.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" type="text/css" href="principal_IndexSV.css">
+    <link rel="stylesheet" href="principal/video.css">
+    <script src="principal/video.js"></script>
+   
+    <title>Inicio</title>
+</head>
+<body>
+<!-- Menu -->
+<div class="menu">
+        <img class="logo" src="../imagenes/logo.png" alt="" />
+        <!-- <h2 class="nombre">PETSTORY</h2> -->
+        <ul align="right">
+            <li style="color:#6cda1a;"> <b>Bienvenid@ </b></li>
+            <li> <b><a href="/">Inicio</a></b></li>
+            <li> <b><a href="/">Lecciones</a></b></li>
+            <li> <b><a href="/">Videos</a></b></li>
+            <li> <b><a href="/">Juegos</a></b></li>
+            <li> <b><a href="/">Registrarse/Iniciar Sesi&#243;n</a></b></li>
+        </ul>
+    </div>
+    <br><br><br>
+
+
+<div id="slider">
+        <input type="radio" name="slider" id="slide1" checked>
+        <input type="radio" name="slider" id="slide2">
+        <input type="radio" name="slider" id="slide3">
+        <input type="radio" name="slider" id="slide4">
+   <div id="slides">
+     <div id="overflow">
+      <div class="inner">
+       <div class="slide slide_1">
+        <div class="slide-content">
+         <h2>Conducir requiere atenci&#243;n,</h2>
+         <p style="font-size:22px;"><b> como cuando juegas. <span style="color:#443d38">&#33;No te distraigas!</b></span> </p>
+        </div>
+       </div>  <!-- Slide 2 -->
+       <div class="slide slide_2">
+        <div class="slide-content">
+         <h2 style="color:#8bdf6f">Usar tu tel&#233;fono mientras conduces,</h2>
+         <p style="color:#66fb34; font-size:22px;"><b>es como manejar con los ojos cerrados.</b> </p> 
+        </div>
+       </div> <!-- Slide 3-->
+       <div class="slide slide_3">
+        <div class="slide-content">
+         <h2 style="color:#8ad671;">Beber y conducir no es un juego. </h2>
+         <p style="color:#f5de4b; font-size:25px;"><b>&#33;Nunca lo hagas!</b></p>
+        </div>
+       </div> <!-- -->
+       <div class="slide slide_4">
+        <div class="slide-content">
+         <h2 style="color:#a9de79;">Si miras tu tel&#233;fono al caminar,</h2>
+         <p style="color:#205647;"><b>es como jugar a ciegas &#33;Muy peligroso!</b></p>
+        </div>
+       </div> <!-- -->
+      </div>
+     </div>
+   </div>
+
+   <div id="controls">
+     <label for="slide1"></label>
+     <label for="slide2"></label>
+     <label for="slide3"></label>
+     <label for="slide4"></label>
+   </div>
+   <div id="bullets">
+     <label for="slide1"></label>
+     <label for="slide2"></label>
+     <label for="slide3"></label>
+     <label for="slide4"></label>
+   </div>
+</div>
+
+
+</body>
